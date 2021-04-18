602d6515cee177c825d3b9e208571ca5164efa1e
SungYubJung <sungyup5003@gmail.com>
2021-04-18 14:05:25 +0900

    add Update in local B

--- main.py
+++ main.py
@@ -4,6 +4,6 @@
 print("Hello World 1 in team leader server")
 
 # add function below this line
-print("Update in local B")
-
 print("Hello World 2 in team leader server")
+
+print("Update in local B")
